03edb41222ee1767843157976bd1fda7fc127df1
Anthony <93596000+AnthonyHartog@users.noreply.github.com>
2022-04-17 14:22:22 +0200

    materiaal css #32 , materiaal html #31 , materiaal plaatjes #30 , materiaal texten #29
    Deze opdrachten zijn afgerond, heb eerst de basis html gemaakt, daarna de plaatjes en texten toegevoegd en toen de css goed gezet.

---
+++ material.php
@@ -0,0 +1,49 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<head>
+  <meta charset="utf-8">
+  <title></title>
+  <meta name="description" content="">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+
+  <meta property="og:title" content="">
+  <meta property="og:type" content="">
+  <meta property="og:url" content="">
+  <meta property="og:image" content="">
+
+  <link rel="manifest" href="site.webmanifest">
+  <link rel="apple-touch-icon" href="icon.png">
+  <!-- Place favicon.ico in the root directory -->
+
+  <link rel="stylesheet" href="css/normalize.css">
+  <link rel="stylesheet" href="css/main.css">
+
+  <meta name="theme-color" content="#fafafa">
+</head>
+
+<body>
+  <script src="js/vendor/modernizr-3.11.2.min.js"></script>
+  <script src="js/plugins.js"></script>
+  <script src="js/main.js"></script>
+    <header>  
+      <?php require_once'header.php' ?>
+    </header>
+    <main>  
+      <div class="wrapper">
+        <div class="info-material">
+          <img class="plaatjematerial" src="img/plaatje5.jpg">
+          <p class="materialtext">Het eerste USP die wij voor deze achtbaan hebben ontwikkeld is een nieuwe metaal messing die nog nooit eerder in deze industrie gebruikt is. Door een menging te maken van 69% staal, 20% nikkel, en 11% koper hebben een nieuwe messing ontwikkeld die wij nu MagnaAlloy noemen. Met deze nieuwe MagnaAlloy maken wij een aantal onderdelen die de meeste stress ondergaan in de achtbaan, waardoor deze nog meer G-krachten kunnen weerstaan, en langer meegaan dan andere stalen. We hebben enorm geprobeerd de kosten zo laag mogelijk te houden, dat de onderdelen gemiddeld slechts 8% duurder uitvallen dan andere soortgelijke onderdelen, zonder kwaliteitsverlies. </p>
+        </div>
+        <div class="info-material">
+          <img class="plaatjematerial" src="img/plaatje5.jpg">
+          <p class="materialtext">Onze tests bewijzen ook dat de onderdelen tot 25% langer meegaan, dus dit zou uiteindelijk ook in kosten moeten besparen. Ook zorgt dit ervoor dat een aantal designs voor achtbanen die eerder niet mogelijk waren nu wel mogelijk zijn.</p>
+        </div>
+      </div>
+    </main>
+    <footer>  
+      <?php require_once'footer.php'?>
+    </footer>
+  </body>
+
+</html>
