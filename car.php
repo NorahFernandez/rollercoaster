81e05cc44c3279ca4e59e361f6c4760482c541df
DirtKingy <92637772+DirtKingy@users.noreply.github.com>
2022-04-20 09:50:52 +0200

    added a main in car.php

--- car.php
+++ car.php
@@ -30,7 +30,10 @@
       <?php require_once'header.php' ?>
     </header>
     <main>  
-
+      <div class="car">
+        <h1><strong>Karretjes</strong></h1>
+        <p>Met onze MagnaCarts die ook speciaal werken op onze nieuw ontwikkelde MagnaAlloy onderdelen, kan elk pretpark het chassis naar wens aanpassen. Wij hebben een frame ontwikkeld die wij zo klein mogelijk hebben gehouden, zodat de rest van de kar helemaal zelf door het team ontworpen en ontwikkeld kan worden. Ook leveren wij de digitale bestanden waar deze ontwerpen op gebaseerd kunnen worden, zodat het team zo snel mogelijk aan de slag kan gaan unieke ontwerpen te creëren. Omdat ook deze karretjes gebruik maken van MagnaAlloy, zijn ze dus ook bestand tegen de extra G-krachten die deze techniek mogelijk maakt. Zo zijn onze producten dus breed inzetbaar, en zorgen wij voor de beste vooruitgang in de achtbaantechniek van het afgelopen decennia.</p>
+      </div>
     </main>
     <footer>  
       <?php require_once'footer.php'?>
